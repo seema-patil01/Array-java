a8886bed0eed3c1b29b8f16774ec279725b926c4
seema patil <seemampatil22@gmail.com>
2025-08-08 21:20:16 +0530

    Add files via upload
    Static Array

---
+++ Static.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Static {
+    public static void main(String[] args) {
+        int[][]a={{1,2,3},{3,4},{1,2,3,4,5}};
+        for(int[]n:a){
+            System.out.println(Arrays.toString(n)+"\n");
+        }
+    }
+}
