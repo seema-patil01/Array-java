7fc7c46566c0856691c0ded27c4d36c6cda453db
seema patil <seemampatil22@gmail.com>
2025-08-15 14:19:13 +0530

    Add files via upload
        Creating and Initializing an Array

---
+++ Initalize.java
@@ -0,0 +1,6 @@
+public class Initalize {
+   public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5};
+        System.out.println("Array created with elements: " + java.util.Arrays.toString(myArray));
+    }  
+}
