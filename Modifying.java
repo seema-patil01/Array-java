f27f708f87dd0f3c1ecf74e3b5bf4d468078e223
seema patil <seemampatil22@gmail.com>
2025-08-16 12:26:23 +0530

    Add files via upload
        Modifying Elements of an Array

---
+++ Modifying.java
@@ -0,0 +1,7 @@
+public class Modifying {
+ public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 5};
+        myArray[1] = 10;
+        System.out.println("Array after modification: " + java.util.Arrays.toString(myArray));
+    }    
+}
