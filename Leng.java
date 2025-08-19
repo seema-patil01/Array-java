5b61c1a5493371ca98bbc344383dc4043260174d
seema patil <seemampatil22@gmail.com>
2025-08-19 09:17:25 +0530

    Add files via upload
    Finding the Length of an Array

---
+++ Leng.java
@@ -0,0 +1,7 @@
+public class Leng {
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 3, 4, 6};
+        System.out.println("The length of the array is: " + myArray.length);
+    }
+}
+
