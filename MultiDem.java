116bbb11e4f0d0864e2ce38afc64e2133f05a129
seema patil <seemampatil22@gmail.com>
2025-08-21 10:12:29 +0530

    Add files via upload
    Multi-dimensional Arrays

---
+++ MultiDem.java
@@ -0,0 +1,8 @@
+public class MultiDem {
+   
+    public static void main(String[] args) {
+        int[][] multiArray = { {1, 2}, {3, 4}, {5, 6} };
+        System.out.println("Element at row 1 column 1: " + multiArray[0][0]);
+    }
+}
+
