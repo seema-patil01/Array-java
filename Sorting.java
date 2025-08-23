7e8ada008144c2127e0ced42aa02c3d4b2bee1c5
seema patil <seemampatil22@gmail.com>
2025-08-23 09:43:53 +0530

    Add files via upload
      Sorting Arrays

---
+++ Sorting.java
@@ -0,0 +1,8 @@
+public class Sorting {
+    public static void main(String[] args) {
+        int[] myArray = {5, 3, 4, 1, 2};
+        java.util.Arrays.sort(myArray);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(myArray));
+    }
+}
+
