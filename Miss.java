ed40130cd36700e0ecc289d23c00321ff9463330
seema patil <seemampatil22@gmail.com>
2025-08-30 12:39:24 +0530

    Add files via upload
    missing  number in Array

---
+++ Miss.java
@@ -0,0 +1,17 @@
+public class Miss {
+   
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 5}; 
+        int n = arr.length + 1;   
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int num : arr) {
+            actualSum += num;
+        }
+        
+        int missing = expectedSum - actualSum;
+        System.out.println("Missing number: " + missing);
+    }
+}
+
+
