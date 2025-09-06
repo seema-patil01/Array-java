c1b769ba196b39e0d6794496ba13b57eab6548cd
seema patil <seemampatil22@gmail.com>
2025-09-06 13:58:39 +0530

    Add files via upload
    Sort the Elements of an Array in Ascending Order

---
+++ Asend.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Asend {
+  
+  	public static void main(String[] args) {
+      
+        int[] arr = new int[] { -2, 0, 1, 3, -1, 2 };
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
